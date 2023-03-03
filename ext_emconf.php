733ff2e2dcefce296ee23df2f2ea239269443bfe
Nilesh Malankiya <nilesh@nitsantech.com>
2023-03-03 10:54:15 +0530

    :tada: [TODO] Initial Upload with working feature

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ns_migration".
+ *
+ * 
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF['ns_migration'] = array (
+  'title' => '[NITSAN] Content Migration',
+  'description' => 'Migrate your table',
+  'category' => 'templates',
+  'version' => '1.0.0',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Nilesh Malankiya',
+  'author_email' => 'nilesh@nitsantech.com',
+  'author_company' => 'NITSAN Technologies Pvt Ltd',
+);
+
